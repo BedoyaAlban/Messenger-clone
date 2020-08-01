637d9cc76a34e77fec710979158205ad551a341a
BedoyaAlban <a.bedoya@outlook.fr>
2020-08-01 10:56:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = event => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Messenger Clone with firebase</h1>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+
+      {messages.map(message => (
+        <p>{message}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;

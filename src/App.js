4f7ff18f3ad770526f9b2c8a0206b13cbd4d4cbc
BedoyaAlban <a.bedoya@outlook.fr>
2020-08-02 11:02:02 +0200

    add message component + style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    { username: "Alban", text: "hey guys" },
+    { username: "Tom", text: "whats up" }
+  ]);
+  const [username, setUserName] = useState("");
+
+  useEffect(() => {
+    setUserName(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = event => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
       <h1>Messenger Clone with firebase</h1>
+      <h2>Welcome {username}</h2>
       <form>
-        <input value={input} onChange={event => setInput(event.target.value)} />
-        <button type="submit" onClick={sendMessage}>
-          Send Message
-        </button>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={event => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            type="submit"
+            color="primary"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
       </form>
 
       {messages.map(message => (
-        <p>{message}</p>
+        <Message username={username} textMessage={message} />
       ))}
     </div>
   );
---
+++ src/Message.js
@@ -0,0 +1,20 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
+import "./Message.css";
+
+function Message({ textMessage, username }) {
+  const isUser = username === textMessage.username;
+  return (
+    <div className={`message ${isUser && "message_user"}`}>
+      <Card className={isUser ? "message_userCard" : "message_noneUserCard"}>
+        <CardContent>
+          <Typography color="white" variant="h5" component="h2">
+            {textMessage.username}: {textMessage.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Message;
